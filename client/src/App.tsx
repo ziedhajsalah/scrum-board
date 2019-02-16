fb17fa437cea9adaeb0ca46bb32d1cf8c73f627b
zied hajsalah <ziedhajsalah@gmail.com>
2019-02-16 18:11:51 +0100

    show loader while fetching

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { ScrumBoard } from './ScrumBoard';
 import { getStories, updateStory, createStory } from './api';
+import { ReactComponent as Loader } from './loader.svg';
 
 export type Status = 'notStarted' | 'inProgress' | 'done';
 
@@ -44,94 +45,117 @@ const initialStories: Story[] = [];
 
 export default function App() {
   const [stories, dispatch] = React.useReducer(storiesReducer, initialStories);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
     getStories().then((stories) => {
       dispatch({ type: setStories, stories });
+      setIsLoading(false);
     });
   }, []);
 
   return (
-    <ScrumBoard
-      stories={stories}
-      moveSubTask={(
-        source: any,
-        destination: any,
-        draggableId: any,
-        story: any,
-        storyIndex: any
-      ) => {
-        if (!destination) {
-          return;
-        }
+    <>
+      <input
+        checked={isLoading}
+        readOnly
+        type="checkbox"
+        id="modal-control"
+        className="modal"
+      />
+      <div>
+        <div className="card" style={{ width: '42px' }}>
+          <Loader height={42} width={42} />
+        </div>
+      </div>
+      <ScrumBoard
+        stories={stories}
+        moveSubTask={(
+          source: any,
+          destination: any,
+          draggableId: any,
+          story: any,
+          storyIndex: any
+        ) => {
+          if (!destination) {
+            return;
+          }
 
-        const fromInProgressToNotStarted =
-          source.droppableId === 'inProgress' &&
-          destination.droppableId === 'notStarted';
+          const fromInProgressToNotStarted =
+            source.droppableId === 'inProgress' &&
+            destination.droppableId === 'notStarted';
 
-        const fromDoneToInProgress =
-          source.droppableId === 'done' &&
-          destination.droppableId === 'inProgress';
+          const fromDoneToInProgress =
+            source.droppableId === 'done' &&
+            destination.droppableId === 'inProgress';
 
-        const fromDoneToNotStarted =
-          source.droppableId === 'done' &&
-          destination.droppableId === 'notStarted';
+          const fromDoneToNotStarted =
+            source.droppableId === 'done' &&
+            destination.droppableId === 'notStarted';
 
-        if (
-          fromDoneToInProgress ||
-          fromInProgressToNotStarted ||
-          fromDoneToNotStarted
-        ) {
-          return;
-        }
+          if (
+            fromDoneToInProgress ||
+            fromInProgressToNotStarted ||
+            fromDoneToNotStarted
+          ) {
+            return;
+          }
 
-        const draggedSubTaskIdx = story.subTasks.findIndex(
-          (st: SubTask) => st._id === draggableId
-        );
-        const draggedSubTask = story.subTasks[draggedSubTaskIdx];
-        const updatedStory = {
-          ...story,
-          subTasks: [
-            ...story.subTasks.slice(0, draggedSubTaskIdx),
-            {
-              ...draggedSubTask,
-              status: destination.droppableId as Status
-            },
-            ...story.subTasks.slice(draggedSubTaskIdx + 1)
-          ]
-        };
+          const draggedSubTaskIdx = story.subTasks.findIndex(
+            (st: SubTask) => st._id === draggableId
+          );
+          const draggedSubTask = story.subTasks[draggedSubTaskIdx];
+          const updatedStory = {
+            ...story,
+            subTasks: [
+              ...story.subTasks.slice(0, draggedSubTaskIdx),
+              {
+                ...draggedSubTask,
+                status: destination.droppableId as Status
+              },
+              ...story.subTasks.slice(draggedSubTaskIdx + 1)
+            ]
+          };
 
-        updateStory(updatedStory).then((data) => {
-          dispatch({
-            type: moveSubtask,
-            updatedStory: data,
-            storyIndex
+          setIsLoading(true);
+          updateStory(updatedStory).then((data) => {
+            dispatch({
+              type: moveSubtask,
+              updatedStory: data,
+              storyIndex
+            });
+            setIsLoading(false);
           });
-        });
-      }}
-      addSubTask={(story: Story, SubtaskName: string, storyIndex: number) => {
-        updateStory({
-          ...story,
-          subTasks: [
-            ...story.subTasks,
-            {
-              name: SubtaskName,
-              status: 'notStarted'
-            }
-          ]
-        }).then((updatedStory) => {
-          dispatch({
-            type: addSubtask,
-            updatedStory,
-            storyIndex
+        }}
+        addSubTask={(story: Story, SubtaskName: string, storyIndex: number) => {
+          setIsLoading(true);
+          updateStory({
+            ...story,
+            subTasks: [
+              ...story.subTasks,
+              {
+                name: SubtaskName,
+                status: 'notStarted'
+              }
+            ]
+          }).then((updatedStory) => {
+            dispatch({
+              type: addSubtask,
+              updatedStory,
+              storyIndex
+            });
+            setIsLoading(false);
           });
-        });
-      }}
-      addStory={(storyName: string) => {
-        createStory({ name: storyName, subTasks: [] }).then((newStory) => {
-          dispatch({ type: addStory, newStory });
-        });
-      }}
-    />
+        }}
+        addStory={(storyName: string) => {
+          setIsLoading(true);
+          createStory({ name: storyName, subTasks: [] }).then((newStory) => {
+            dispatch({ type: addStory, newStory });
+            setIsLoading(false);
+          });
+        }}
+      />
+    </>
   );
 }
